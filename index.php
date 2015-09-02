f0c53e070633d69974ae5718d53dc2672bdd961a
jid <jid@st-andrews.ac.uk>
2015-09-02 08:28:27 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Postcode Finder</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        .container {
+            background-image: url("images/image.jpg");
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-position: center;
+            padding-top: 150px;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        .black {
+            color: #000000;
+        }
+
+        p {
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+
+        button {
+            margin-bottom: 20px;
+        }
+
+        .alert {
+            margin-top: 20px;
+            display: none;
+        }
+
+        .whitebackground {
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 20px;
+        }
+
+    </style>
+
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3 center whitebackground" >
+            <h1 class="center black">Postcode Finder</h1>
+
+            <p class="lead center black">Enter any address to find your postcode</p>
+
+            <form>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="address" id="address" placeholder="Eg. 55 Fake Street, Any City" />
+                </div>
+
+                <button id="findMyPostcode" class="btn btn-success btn-lg">Find My Postcode</button>
+            </form>
+
+            <div id="success" class="alert alert-success">
+                Success!
+            </div>
+
+            <div id="fail" class="alert alert-danger">
+                Could not find postcode data for that address
+            </div>
+
+            <div id="fail2" class="alert alert-danger">
+                Could not connect to server
+            </div>
+
+            <div id="noCity" class="alert alert-danger">
+                Please enter an address!
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>-->
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<script>
+    $("#findMyPostcode").click(function(event) {
+
+        var result=0;
+
+        $(".alert").hide();
+
+        event.preventDefault();
+
+        $.ajax({
+            type: "GET",
+            url:"https://maps.googleapis.com/maps/api/geocode/xml?address=" + encodeURIComponent($('#address').val()) +
+            "&key=AIzaSyDqqliQKNvhBjkyo7LXVCZ2lGT7DRWsbaU",
+            dataType:"xml",
+            success: processXML,
+            error: error
+        });
+
+        function error() {
+            $("#fail2").fadeIn();
+        }
+
+        function processXML(xml) {
+            $(xml).find("address_component").each(function(){
+               if ($(this).find("type").text() == "postal_code") {
+                   $("#success").html("The postcode you need is " + $(this).find('long_name').text()).fadeIn();
+
+                   result=1;
+               }
+            });
+
+            if (result == 0) {
+                $("#fail").fadeIn();
+            }
+
+        }
+    })
+</script>
+
+</body>
+</html>
